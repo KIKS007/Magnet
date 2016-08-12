2a5f0147c6c34b94b2daa96a058cc15288711255
Keryann Malacchina <keryann@gmail.com>
2016-08-12 14:34:45 +0200

    Wrap DONE

--- Assets/Scripts/GlobalMethods.cs
+++ Assets/Scripts/GlobalMethods.cs
@@ -139,5 +139,5 @@ public class GlobalMethods : Singleton<GlobalMethods>
 			}
 		}
 	}
-
+		
 }
---
+++ Assets/Scripts/Mode Manager/WrapModeManager.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class WrapModeManager : MonoBehaviour 
+{
+	[Header ("Wrap Settings")]
+	public int timerDuration = 300;
+	public float timeBetweenSpawn = 2;
+
+	[Header ("Timer")]
+	public float timer;
+	public string timerClock;
+
+	// Use this for initialization
+	void Start () 
+	{
+		timer = timerDuration;
+
+		StartCoroutine (StartTimer ());
+	}
+
+	IEnumerator StartTimer ()
+	{
+		while(GlobalVariables.Instance.GameOver == true || GlobalVariables.Instance.GamePaused == true)
+		{
+			yield return null;
+		}
+
+		StartCoroutine (Timer ());
+	}
+
+	IEnumerator Timer ()
+	{
+		yield return null;
+
+		while(GlobalVariables.Instance.GameOver == true || GlobalVariables.Instance.GamePaused == true)
+		{
+			yield return null;
+		}
+
+		timer -= Time.deltaTime;
+
+		string minutes = Mathf.Floor(timer / 60).ToString("0");
+		string seconds = Mathf.Floor(timer % 60).ToString("00");
+
+		timerClock = minutes + ":" + seconds;
+
+		transform.GetChild (0).GetChild (0).GetComponent<Text> ().text = timerClock;
+
+
+		if(timer > 0.01f)
+			StartCoroutine (Timer ());
+
+		else
+		{
+			GameEnded ();
+			transform.GetChild (0).GetChild (0).GetComponent<Text> ().text = "00:00";
+		}
+	}
+
+	void GameEnded ()
+	{
+		GlobalVariables.Instance.GameOver = true;
+	}
+}
