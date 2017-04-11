2ec8370eb164b03830e5c9db67c17094ca41b11f
Keryann Malacchina <keryannm@gmail.com>
2017-04-11 11:30:06 +0200

    4.8.1 : Fixed Collision Particles

---
+++ Assets/Scripts/Others/ArenaDeadzones.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArenaDeadzones : MonoBehaviour 
+{
+	[Header ("Columns")]
+	public Transform[] frontColumns = new Transform[27];
+	public Transform[] backColumns = new Transform[27];
+	public Transform[] rightColumns = new Transform[17];
+	public Transform[] leftColumns = new Transform[17];
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
