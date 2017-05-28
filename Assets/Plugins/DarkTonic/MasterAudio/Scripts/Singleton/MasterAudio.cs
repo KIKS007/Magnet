c66b5430e776c9157c035da1f566589208579bc8
Keryann Malacchina <keryannm@gmail.com>
2017-05-28 17:29:55 +0200

    6.7.1 : Unity 5.6.1f1

--- Assets/Plugins/DarkTonic/MasterAudio/Scripts/Singleton/MasterAudio.cs
+++ Assets/Plugins/DarkTonic/MasterAudio/Scripts/Singleton/MasterAudio.cs
@@ -2722,7 +2722,7 @@ namespace DarkTonic.MasterAudio {
 
             var ma = Instance;
 
-            if (Instance.Trans.FindChild(groupName) != null) {
+            if (Instance.Trans.Find(groupName) != null) {
                 if (errorOnExisting) {
                     Debug.LogError("Cannot add a new Sound Group named '" + groupName +
                                    "' because there is already a Sound Group of that name.");
@@ -3216,7 +3216,7 @@ namespace DarkTonic.MasterAudio {
         /// <param name="sType">Name of Sound Group</param>
         /// <returns>Transform</returns>
         public static Transform FindGroupTransform(string sType) {
-            var grp = Instance.Trans.FindChild(sType);
+            var grp = Instance.Trans.Find(sType);
             if (grp != null) {
                 return grp;
             }
@@ -3224,7 +3224,7 @@ namespace DarkTonic.MasterAudio {
             var dgscs = FindObjectsOfType<DynamicSoundGroupCreator>();
             for (var i = 0; i < dgscs.Count(); i++) {
                 var d = dgscs[i];
-                grp = d.transform.FindChild(sType);
+                grp = d.transform.Find(sType);
 
                 if (grp != null) {
                     return grp;
