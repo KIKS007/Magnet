4de099d5a907ef96a9ab071f1fe8e366d818bff6
Keryann Malacchina <keryannm@gmail.com>
2017-10-04 15:08:19 +0200

    9.4.2 : Fix FX AI

--- Assets/Scripts/AI/Other/AIFXAnimations.cs
+++ Assets/Scripts/AI/Other/AIFXAnimations.cs
@@ -2,66 +2,70 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AIFXAnimations : PlayersFXAnimations 
+public class AIFXAnimations : PlayersFXAnimations
 {
-	[Header ("AI")]
-	public Transform trails;
-	public Transform meshes;
-	public Transform dashDispo;
-	public Transform dash;
+    [Header("AI")]
+    public Transform trails;
+    public Transform meshes;
+    public Transform dashDispo;
+    public Transform dash;
 
-	protected AIGameplay aiScript;
+    protected AIGameplay aiScript;
 
-	protected override void Start ()
-	{
-		playerScript = GetComponent<PlayersGameplay> ();
-		playerSoundsScript = GetComponent<PlayersSounds> ();
+    protected override void Start()
+    {
+        playerScript = GetComponent<PlayersGameplay>();
+        playerSoundsScript = GetComponent<PlayersSounds>();
 
-		playerScript.OnShoot += ShootFX;
-		playerScript.OnDashAvailable += DashAvailableFX;
-		playerScript.OnDash += StopDashAvailable;
-		playerScript.OnStun += ()=> StartCoroutine (StunFX ());
-		playerScript.OnDash += EnableDashFX;
-		playerScript.OnDeath += RemoveAttractionRepulsionFX;
-		playerScript.OnSafe += () => StartCoroutine (SafeFX ());
+        playerScript.OnShoot += ShootFX;
+        playerScript.OnDashAvailable += DashAvailableFX;
+        playerScript.OnDash += StopDashAvailable;
+        playerScript.OnStun += () => StartCoroutine(StunFX());
+        playerScript.OnDash += EnableDashFX;
+        playerScript.OnDeath += RemoveAttractionRepulsionFX;
+        playerScript.OnSafe += () => StartCoroutine(SafeFX());
 
-		aiScript = (AIGameplay) playerScript;
-	}
+        aiScript = (AIGameplay)playerScript;
 
-	public void AISetup ()
-	{
-		if(aiScript == null)
-		{
-			playerScript = GetComponent<PlayersGameplay> ();
-			aiScript = (AIGameplay) playerScript;
-		}
+        SetupMaterials();
 
-		SetupPlayer (trails);
+        Setup();
+    }
 
-		playerMesh = SetupPlayer (meshes).transform;
-		dashAvailableFX = SetupPlayer (dashDispo).GetComponent<ParticleSystem> ();
-		dashFX = SetupPlayer (dash).GetComponent<ParticleSystem> ();
+    public void AISetup()
+    {
+        if (aiScript == null)
+        {
+            playerScript = GetComponent<PlayersGameplay>();
+            aiScript = (AIGameplay)playerScript;
+        }
 
-		playerColorMaterial = GlobalVariables.Instance.playersMaterials [(int)aiScript.playerName];
+        SetupPlayer(trails);
 
-		base.Setup ();
-	}
+        playerMesh = SetupPlayer(meshes).transform;
+        dashAvailableFX = SetupPlayer(dashDispo).GetComponent<ParticleSystem>();
+        dashFX = SetupPlayer(dash).GetComponent<ParticleSystem>();
 
-	GameObject SetupPlayer (Transform t)
-	{
-		GameObject g = null;
+        playerColorMaterial = GlobalVariables.Instance.playersMaterials[(int)aiScript.playerName];
 
-		for (int i = 0; i < t.childCount; i++)
-		{
-			if (i != (int)aiScript.playerName)
-				t.GetChild (i).gameObject.SetActive (false);
-			else
-			{
-				g = t.GetChild (i).gameObject;
-				t.GetChild (i).gameObject.SetActive (true);
-			}
-		}
+        base.Setup();
+    }
 
-		return g;
-	}
+    GameObject SetupPlayer(Transform t)
+    {
+        GameObject g = null;
+
+        for (int i = 0; i < t.childCount; i++)
+        {
+            if (i != (int)aiScript.playerName)
+                t.GetChild(i).gameObject.SetActive(false);
+            else
+            {
+                g = t.GetChild(i).gameObject;
+                t.GetChild(i).gameObject.SetActive(true);
+            }
+        }
+
+        return g;
+    }
 }
