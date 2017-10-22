c7647e4b73aca567c3eb89733e0bfc9176f4ef9f
Keryann Malacchina <keryannm@gmail.com>
2017-10-22 16:00:41 +0200

    Blue Almost FInished Spotlight to Do

--- Assets/Scripts/Others/SongBanner.cs
+++ Assets/Scripts/Others/SongBanner.cs
@@ -5,93 +5,94 @@ using UnityEngine.UI;
 using DarkTonic.MasterAudio;
 using DG.Tweening;
 
-public class SongBanner : MonoBehaviour 
+public class SongBanner : MonoBehaviour
 {
-	[Header ("Banner")]
-	public Ease bannerEase = Ease.Linear;
-	public float bannerSpeed;
-	public float bannerPauseDuration;
+    [Header("Banner")]
+    public Ease bannerEase = Ease.Linear;
+    public float bannerSpeed;
+    public float bannerPauseDuration;
 
-	private RectTransform maskBannerRect;
-	private RectTransform songRect;
-	private Text songText;
+    private RectTransform maskBannerRect;
+    private RectTransform songRect;
+    private Text songText;
 
-	public float centerPosition;
-	public float leftPosition;
-	public float rightPosition;
+    public float centerPosition;
+    public float leftPosition;
+    public float rightPosition;
 
-	private PlaylistController playlistController;
+    private PlaylistController playlistController;
 
-	// Use this for initialization
-	void Awake () 
-	{
-		playlistController = FindObjectOfType<PlaylistController> ();
+    // Use this for initialization
+    void Awake()
+    {
+        playlistController = FindObjectOfType<PlaylistController>();
 
-		maskBannerRect = GetComponent<RectTransform> ();
-		songRect = transform.GetChild (0).GetComponent<RectTransform> ();
-		songText = transform.GetChild (0).GetComponent<Text> ();
+        maskBannerRect = GetComponent<RectTransform>();
+        songRect = transform.GetChild(0).GetComponent<RectTransform>();
+        songText = transform.GetChild(0).GetComponent<Text>();
 
-		playlistController.SongChanged += SongChangedVoid;
+        playlistController.SongChanged += SongChangedVoid;
+    }
 
-		StartCoroutine (WaitForPlaylist ());
-	}
+    void OnEnable()
+    {
+        playlistController = FindObjectOfType<PlaylistController>();
+        songText = transform.GetChild(0).GetComponent<Text>();
 
-	void OnEnable ()
-	{
-		playlistController = FindObjectOfType<PlaylistController> ();
-		songText = transform.GetChild (0).GetComponent<Text> ();
+        if (playlistController.CurrentPlaylistClip)
+            songText.text = playlistController.CurrentPlaylistClip.name;
 
-		if(playlistController.CurrentPlaylistClip)
-			songText.text = playlistController.CurrentPlaylistClip.name;
-	}
+        StartCoroutine(WaitForPlaylist());
+    }
 
-	void OnDestroy ()
-	{
-		playlistController.SongChanged -= SongChangedVoid;
-	}
+    void OnDestroy()
+    {
+        playlistController.SongChanged -= SongChangedVoid;
+    }
 
-	void SongChangedVoid (string name)
-	{
-		StopAllCoroutines ();
-		StartCoroutine (SongChanged (name));
-	}
+    void SongChangedVoid(string name)
+    {
+        StopAllCoroutines();
+        StartCoroutine(SongChanged(name));
+    }
 
-	IEnumerator WaitForPlaylist ()
-	{
-		yield return new WaitUntil (() => playlistController.CurrentPlaylistClip != null);
+    IEnumerator WaitForPlaylist()
+    {
+        yield return new WaitUntil(() => playlistController.CurrentPlaylistClip != null);
 
-		StartCoroutine (SongChanged (playlistController.CurrentPlaylistClip.name));
-	}
+        StartCoroutine(SongChanged(playlistController.CurrentPlaylistClip.name));
+    }
 
-	IEnumerator SongChanged (string newSongName)
-	{
-		DOTween.Kill ("SongBanner");
+    IEnumerator SongChanged(string newSongName)
+    {
+        DOTween.Kill("SongBanner");
 
-		Tween tween = songRect.DOAnchorPosX (leftPosition, 10000f).SetSpeedBased ().SetUpdate (true);
-		yield return tween.WaitForCompletion ();
+        Tween tween = songRect.DOAnchorPosX(leftPosition, 10000f).SetSpeedBased().SetUpdate(true);
+        yield return tween.WaitForCompletion();
 
-		songText.text = newSongName;
+        songText.text = newSongName;
 
-		yield return new WaitForEndOfFrame ();
+        yield return new WaitForEndOfFrame();
 
-		centerPosition = 0;
-		leftPosition = -songRect.sizeDelta.x;
-		rightPosition = maskBannerRect.sizeDelta.x;
+        centerPosition = 0;
+        leftPosition = -songRect.sizeDelta.x;
+        rightPosition = maskBannerRect.sizeDelta.x;
 
-		Show ();
-	}
+        Show();
+    }
 
-	void Show ()
-	{
-		songRect.anchoredPosition = new Vector2 (rightPosition, songRect.anchoredPosition.y);
+    void Show()
+    {
+        songRect.anchoredPosition = new Vector2(rightPosition, songRect.anchoredPosition.y);
 		
-		songRect.DOAnchorPosX (centerPosition, bannerSpeed).SetSpeedBased ().SetEase (bannerEase).OnComplete (()=> {
-			DOVirtual.DelayedCall (bannerPauseDuration, ()=> Hide ()).SetId ("SongBanner").SetUpdate (true);
-		}).SetId ("SongBanner").SetUpdate (true);
-	}
+        songRect.DOAnchorPosX(centerPosition, bannerSpeed).SetSpeedBased().SetEase(bannerEase).OnComplete(() =>
+            {
+                DOVirtual.DelayedCall(bannerPauseDuration, () => Hide()).SetId("SongBanner").SetUpdate(true);
+            }).SetId("SongBanner").SetUpdate(true);
+    }
 
-	void Hide ()
-	{
-		songRect.DOAnchorPosX (leftPosition, bannerSpeed).SetSpeedBased ().OnComplete (Show).SetId ("SongBanner").SetUpdate (true);
-	}
+    void Hide()
+    {
+        songRect.DOAnchorPosX(leftPosition, bannerSpeed).SetSpeedBased().OnComplete(Show).SetId("SongBanner").SetUpdate(true);
+    }
 }
